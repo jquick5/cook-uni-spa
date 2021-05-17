d28a4bc90b24b8906c2d2fca640b8ee5337ccdb1
jquick5 <jay.quick79@gmail.com>
2021-05-17 15:16:10 -0700

    Added register and login functionality to the server

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('user', UserSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,83 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 //@route POST api/users
 // @desc Resister a user
 // @access Public
-router.post('/', (req, res) => {
-  res.send('Register a user');
-});
+router.post(
+  '/',
+  [
+    check('firstName', 'Please add your first name').not().isEmpty(),
+    check('lastName', 'Please add your last name').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters',
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { userName, firstName, lastName, email, password } = req.body;
+
+    try {
+      let username = await User.findOne({ userName });
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+      if (username) {
+        return res.status(400).json({
+          msg: 'Username already exists, please enter a different username',
+        });
+      }
+
+      user = new User({
+        userName,
+        firstName,
+        lastName,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        },
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  },
+);
 
 module.exports = router;
